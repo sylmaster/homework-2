b266af2a60e869b1971cf72e67f7f6b64f815359
sylmaster <Sylvain.Lambert@lautorite.qc.ca>
2019-08-22 12:18:46 -0400

    XXX

---
+++ code.R
@@ -0,0 +1,10 @@
+install.packages(c("tidyverse", "dslabs"))
+
+library(tidyverse)
+library(dslabs)
+data(murders)
+
+
+murders %>%
+  ggplot(aes(population, total, label = abb, color = region)) +
+  geom_label()
